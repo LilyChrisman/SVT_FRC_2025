3781b45a7496789b0fdc32c01a22d118dae1e924
Teigan Bronec <teiganbronec1@gmail.com>
2025-02-25 15:26:18 -0800

    Added positions and changed lift to be driven by motion magic

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.configs.Slot1Configs;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -18,6 +19,10 @@ public class ArmSubsystem extends SubsystemBase{
         slot1Configs.kD = 0;
 
         armMotor.getConfigurator().apply(slot1Configs);
+    }
+
+    public void periodic(){
+        SmartDashboard.putNumber("Elevator Position", armMotor.getPosition().getValueAsDouble());
     }
 
     public Command armGoToPosCommand(double position){
--- src/main/java/frc/robot/subsystems/ExtakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExtakeSubsystem.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -28,11 +30,23 @@ public class ExtakeSubsystem extends SubsystemBase{
     public final double ARM_EXTAKE_LOW = 20;
 
     public ExtakeSubsystem(){
-        // in init function, set slot 0 gains
-        var slot0Configs = new Slot0Configs();
-        slot0Configs.kP = .4; // An error of 1 rotation results in 2.4 V output
+        var talonFXConfigs = new TalonFXConfiguration();
+
+        // set slot 0 gains
+        var slot0Configs = talonFXConfigs.Slot0;
+        slot0Configs.kS = 0.25; // Add 0.25 V output to overcome static friction
+        slot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+        slot0Configs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
+        slot0Configs.kP = 4.8; // A position error of 2.5 rotations results in 12 V output
         slot0Configs.kI = 0; // no output for integrated error
-        slot0Configs.kD = 0; // A velocity of 1 rps results in 0.1 V output
+        slot0Configs.kD = 0.1; // A velocity error of 1 rps results in 0.1 V output
+
+        // set Motion Magic settings
+        var motionMagicConfigs = talonFXConfigs.MotionMagic;
+        motionMagicConfigs.MotionMagicCruiseVelocity = 80; // Target cruise velocity of 80 rps
+        motionMagicConfigs.MotionMagicAcceleration = 160; // Target acceleration of 160 rps/s (0.5 seconds)
+        motionMagicConfigs.MotionMagicJerk = 1600; // Target jerk of 1600 rps/s/s (0.1 seconds)
+
 
         liftMotor1.getConfigurator().apply(slot0Configs);
         liftMotor2.getConfigurator().apply(slot0Configs);
@@ -43,7 +57,7 @@ public class ExtakeSubsystem extends SubsystemBase{
     }
 
     public Command liftGoToPosCommand(double position){
-        final PositionVoltage m_request = new PositionVoltage(0).withSlot(0);
+        final MotionMagicVoltage m_request = new MotionMagicVoltage(0).withSlot(0);
         final Follower m_Follower = new Follower(31, false);
          return run(() -> {
             System.out.println("Test Print");
