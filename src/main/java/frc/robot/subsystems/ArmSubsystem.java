8baadc17bd3551c73cfde15ad5112090baa00b7f
GwabbaWaba <gwabbawaba@gmail.com>
2025-02-24 16:37:28 -0800

    latest

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,12 +4,19 @@ import com.ctre.phoenix6.configs.Slot1Configs;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase{
     /** Motor for arm on the lift that has the grabber on the end */
     private final TalonFX armMotor = new TalonFX(42, "rio");
+
+    public final double ARM_EXTAKE_L4 = 41;
+    public final double ARM_INTAKE_CORAL = 0;
+    public final double ARM_EXTAKE_L3 = 38.5;
+    public final double ARM_EXTAKE_L2 = 25;
+    public final double ARM_EXTAKE_L1 = 15;
 
     public ArmSubsystem(){
          var slot1Configs = new Slot1Configs();
@@ -18,6 +25,10 @@ public class ArmSubsystem extends SubsystemBase{
         slot1Configs.kD = 0;
 
         armMotor.getConfigurator().apply(slot1Configs);
+    }
+
+    public void periodic(){
+        SmartDashboard.putNumber("Arm Position", armMotor.getPosition().getValueAsDouble());
     }
 
     public Command armGoToPosCommand(double position){
